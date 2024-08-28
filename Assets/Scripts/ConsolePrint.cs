4ce0f885bcbd359e065d564eb12125e3da589907
LeslieLeeGithub <146797669+LeslieLeeGithub@users.noreply.github.com>
2024-08-28 11:42:13 +1000

    80 percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    private int counter = 3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        counter++;
+
+        Debug.Log($"{gameObject.name}:{counter}");
+
+    }
+}
