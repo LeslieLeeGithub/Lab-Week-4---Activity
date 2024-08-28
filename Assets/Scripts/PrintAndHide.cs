7e4f68c301d1a307b8da4f0b674715932ff1bce1
LeslieLeeGithub <146797669+LeslieLeeGithub@users.noreply.github.com>
2024-08-28 12:02:52 +1000

    100

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -9,13 +9,16 @@ public class PrintAndHide : MonoBehaviour
 
     public Renderer rend;
 
+    private int randomBluethreshold;
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomBluethreshold = Random.Range(150,251);
     }
 
     // Update is called once per frame
@@ -25,6 +28,31 @@ public class PrintAndHide : MonoBehaviour
         counter++;
 
         Debug.Log($"{gameObject.name}:{counter}");
+
+
+        if (gameObject.CompareTag("Red") && counter == 100)
+        {
+            gameObject.SetActive(false);
+
+
+        }
+
+
+        if (gameObject.CompareTag("Blue") && counter == randomBluethreshold)
+        {
+            Renderer renderer = GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.enabled = false;
+
+
+            }
+
+
+
+
+        }
 
     }
 }
