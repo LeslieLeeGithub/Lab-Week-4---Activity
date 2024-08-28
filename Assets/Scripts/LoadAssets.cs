cd151d35cf952aae82bc3d6051da4e9f8dbec3cf
LeslieLeeGithub <146797669+LeslieLeeGithub@users.noreply.github.com>
2024-08-28 11:34:34 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -7,12 +7,21 @@ public class LoadAssets : MonoBehaviour
 
     public GameObject redObj;
 
+    [SerializeField]
+    private GameObject blueObj;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
+        // Limit framerate to cinematic 24fps.
+        QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
+        Application.targetFrameRate = 60;
+
+
     }
 
     // Update is called once per frame
